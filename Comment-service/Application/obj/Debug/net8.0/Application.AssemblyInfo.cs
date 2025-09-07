76b2c75119b88c56e3e3a10949ef85203ac71cfc
ngocj <ngocnguyen10072003@gmail.com>
2025-09-07 09:09:19 +0700

    update gitignore

--- Comment-service/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Comment-service/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13867aa96bc2b148be43f4d82dde9cb5d734605e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
